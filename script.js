58f4933a289ef5bd2dea93b742bc65de726fa469
Abdulaziz Shafae <aasshafae@gmail.com>
2025-03-25 05:47:04 +0300

    early popup for add task

--- script.js
+++ script.js
@@ -5,25 +5,3 @@ function dropdownToggle() {
 
 
 
-// Load the modal from addTaskpop.html
-function loadModal() {
-    fetch("addTaskpop.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("modal-container").innerHTML = data;
-        })
-        .catch(error => console.error("Error loading modal:", error));
-}
-
-// Open Modal
-function openModal() {
-    document.getElementById("modal-bg").style.display = "flex";
-}
-
-// Close Modal
-function closeModal() {
-    document.getElementById("modal-bg").style.display = "none";
-}
-
-// Load modal when the page loads
-window.onload = loadModal;
