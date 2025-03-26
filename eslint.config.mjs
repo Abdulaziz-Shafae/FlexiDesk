7cf3d0049ffcd0745f6603143495835b15f9d042
Abdulaziz Shafae <aasshafae@gmail.com>
2025-03-26 03:17:33 +0300

    fixing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,28 @@
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import js from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ['**/*.{js,mjs,cjs,jsx}'],
   },
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ['**/*.{js,mjs,cjs,jsx}'],
     languageOptions: {
       globals: globals.browser,
     },
   },
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ['**/*.{js,mjs,cjs,jsx}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
   },
   pluginReact.configs.flat.recommended,
   {
     settings: {
       react: {
-        version: "detect", // Automatically detect the React version
+        version: 'detect', // Automatically detect the React version
       },
     },
   },
--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
-
 /* eslint-disable no-unused-vars */
 function profiledropdown() {
-    let dropdown = document.getElementById("profileList");
-    dropdown.style.display = dropdown.style.display === "flex" ? "none" : "flex";
+  let dropdown = document.getElementById('profileList');
+  dropdown.style.display = dropdown.style.display === 'flex' ? 'none' : 'flex';
 }
 /* eslint-enable no-unused-vars */
