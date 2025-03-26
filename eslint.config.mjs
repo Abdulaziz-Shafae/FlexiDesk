b0d7c84431f9b82b685684a1b7e705a940ca1cb8
Abdulaziz Shafae <aasshafae@gmail.com>
2025-03-26 03:09:20 +0300

    2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,10 +3,27 @@ import globals from "globals";
 import js from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect", // Automatically detect the React version
+      },
+    },
+  },
 ]);
--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
+
+/* eslint-disable no-unused-vars */
 function profiledropdown() {
     let dropdown = document.getElementById("profileList");
     dropdown.style.display = dropdown.style.display === "flex" ? "none" : "flex";
 }
-
-
-
+/* eslint-enable no-unused-vars */
